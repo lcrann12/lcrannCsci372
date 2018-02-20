e7c2665cd2349d4df5790f926e4a9b68ac69fd51
Lenny Rann <android@yoda.cs.widener.edu>
2018-02-20 15:13:00 -0500

    Revised Loan Calc

--- LoanCalculator/app/src/main/java/com/example/android/loancalculator/LoanCalc.java
+++ LoanCalculator/app/src/main/java/com/example/android/loancalculator/LoanCalc.java
@@ -10,7 +10,6 @@ import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class LoanCalc extends AppCompatActivity {
-
     private EditText costAmount;
     private EditText downAmount;
     private EditText aprAmount;
@@ -32,25 +31,18 @@ public class LoanCalc extends AppCompatActivity {
         downAmount = findViewById(R.id.downAmount);
         aprAmount = findViewById(R.id.aprAmount);
 
+        loanButton = findViewById(R.id.loanButton);
+        leaseButton = findViewById(R.id.leaseButton);
+
         resultView = findViewById(R.id.resultView);
-        resultView.setOnEditorActionListener(
-                new TextView.OnEditorActionListener() {
-                    @Override
-                    public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                        String v = resultView.getText().toString();
-                        int progress = Integer.parseInt(v);
-                        monthSelector.setProgress(progress);
-                        return false;
-                    }
-                }
-        );
         barLabel = findViewById(R.id.barLabel);
+
         monthSelector = findViewById(R.id.monthSelector);
         monthSelector.setOnSeekBarChangeListener(
                 new SeekBar.OnSeekBarChangeListener() {
                     @Override
                     public void onProgressChanged(SeekBar seekBar, int progress, boolean b) {
-                        monthSelector.setProgress(progress);
+                        seekBar.setProgress(progress);
                         barLabel.setText(String.valueOf("Length of Loan: "+progress+""));
                     }
 
@@ -68,20 +60,25 @@ public class LoanCalc extends AppCompatActivity {
 
     }
     public void buttonPressed(View v){
-        int p = monthSelector.getProgress();
-        double mpResult;
+        double p = monthSelector.getProgress();
         String costInput = costAmount.getText().toString();
-        int cI = Integer.parseInt(costInput); //cost of car
+        double cI = Double.parseDouble(costInput); //cost of car
         String downInput = downAmount.getText().toString();
-        int dI = Integer.parseInt(downInput);     //down payment
+        double dI = Double.parseDouble(downInput);     //down payment
         String aprInput = aprAmount.getText().toString();
         double aI = Double.parseDouble(aprInput);  //apr
-        double mr = (aI/12);
+        double mpResult;
+        double mr = ((aI/100)/12);
+        double L = cI-dI;
+
         if(loanButton.isChecked()){
-            mpResult = ((aI/100)/12)*((cI-dI)/(1-(1+Math.pow(mr, -p))));
-        }else{
-            mpResult = ((aI/100)/12)*(((cI/3)-dI)/(1-(1+Math.pow(mr, -36))));
+            mpResult = mr*L/(1-(Math.pow(1+mr, -p)));
+            resultView.setText("$"+(String.format("%.2f", mpResult)+""));
+
+        }else if (leaseButton.isChecked()){
+            mpResult = mr*(L/3)/(1-(Math.pow(1+mr, -36)));
+            resultView.setText("$"+(String.format("%.2f", mpResult)+""));
+
         }
-        resultView.setText("$"+mpResult+"");
     }
 }
